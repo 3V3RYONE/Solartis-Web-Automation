5be404a1789fd70f531a5f8b683606f354929381
3V3RYONE <beleswarprasad@gmail.com>
2022-03-16 19:57:14 +0530

    update deprecated code styles

--- automation_script.py
+++ automation_script.py
@@ -10,42 +10,42 @@ import time
 import openpyxl
 
 def Enrollee_Information(list_data):
-    member_number = browser.find_element_by_name("EAANumber")
+    member_number = browser.find_element(by=By.NAME, value="EAANumber")
     member_number.send_keys(list_data[0])
 
-    first_name = browser.find_element_by_name("firstName")
+    first_name = browser.find_element(by=By.NAME, value="firstName")
     first_name.send_keys(list_data[1].strip())
    
     if list_data[2] is not None:
-        middle_name = browser.find_element_by_name("middleName")
+        middle_name = browser.find_element(by=By.NAME, value="middleName")
         middle_name.send_keys(list_data[2].strip())
 
-    last_name = browser.find_element_by_name("lastName")
+    last_name = browser.find_element(by=By.NAME, value="lastName")
     last_name.send_keys(list_data[3].strip())
     
-    DOB = browser.find_element_by_name("insuredDateOfBirth")
+    DOB = browser.find_element(by=By.NAME, value="insuredDateOfBirth")
     DOB.send_keys(list_data[4].strip())
 
-    Address1 = browser.find_element_by_name("address1")
+    Address1 = browser.find_element(by=By.NAME,value="address1")
     Address1.send_keys(list_data[5].strip())
 
     if list_data[6] is not None:
-        Address2 = browser.find_element_by_name("address2")
+        Address2 = browser.find_element(by=By.NAME, value="address2")
         Address2.send_keys(list_data[6].strip())
 
-    City = browser.find_element_by_name("city")
+    City = browser.find_element(by=By.NAME, value="city")
     City.send_keys(list_data[7].strip())
 
-    State = Select(browser.find_element_by_name("state"))
+    State = Select(browser.find_element(by=By.NAME, value="state"))
     State.select_by_visible_text(list_data[8])
 
-    Zip = browser.find_element_by_name("autocompleteNomeIdInput")
+    Zip = browser.find_element(by=By.NAME,value="autocompleteNomeIdInput")
     Zip.send_keys(str(list_data[9]))
 
-    email = browser.find_element_by_name("email")
+    email = browser.find_element(by=By.NAME,value="email")
     email.send_keys(list_data[10].strip())
 
-    phone = browser.find_element_by_name("phone")
+    phone = browser.find_element(by=By.NAME,value="phone")
     phone.send_keys(list_data[11])
 
     #enrol_date = browser.find_element_by_name("EnrollmentDateInputDate")
@@ -61,8 +61,8 @@ def Enrollee_Information(list_data):
         if 'Spouse' not in list_data[13]:
             plan_name = "planNameItem2"
 
-    plan = browser.find_element_by_name("planNameInput").click()
-    browser.find_element_by_id(plan_name).click()
+    plan = browser.find_element(by=By.NAME,value="planNameInput").click()
+    browser.find_element(by=By.ID,value=plan_name).click()
 
     if  list_data[14]==50000:
         sum_value = "coverageLimitItem0"
@@ -77,8 +77,8 @@ def Enrollee_Information(list_data):
     elif list_data[14]==250000:
         sum_value="coverageLimitItem5"
 
-    Principal_sum = browser.find_element_by_name("coverageLimitInput").click()
-    browser.find_element_by_id(sum_value).click()
+    Principal_sum = browser.find_element(by=By.NAME,value="coverageLimitInput").click()
+    browser.find_element(by=By.ID, value=sum_value).click()
 
     Submit_button1 = WebDriverWait(browser,180).until(EC.element_to_be_clickable((By.ID,"submitCertReq2"))).click() #find_element_by_name("submitCertReq2")
     
@@ -108,16 +108,16 @@ def Beneficiary(list_ben,count_ben):
         last_ben_name.send_keys(list_ben[i][2].strip())
        
         time.sleep(1)
-        relationship = browser.find_element_by_id("relationship")
+        relationship = browser.find_element(by=By.ID,value="relationship")
         #relationship = WebDriverWait(browser,30).until(EC.presence_of_element_located((By.ID,"relationship")))
         relationship.send_keys(list_ben[i][3].strip())
         
-        percentage = browser.find_element_by_id("Beneficiarypercentage")
+        percentage = browser.find_element(by=By.ID,value="Beneficiarypercentage")
         percentage.send_keys(list_ben[i][4])
         percentage.clear()
         percentage.send_keys(list_ben[i][4])
        
-        save = browser.find_element_by_id("j_idt127").click()
+        save = browser.find_element(by=By.ID, value="j_idt127").click()
         time.sleep(3)
    
     continue_ben = WebDriverWait(browser,180).until(EC.element_to_be_clickable((By.ID,"j_idt55"))).click()
@@ -129,29 +129,29 @@ def Dependents(list_dependents,count_dep):
         add_dep = WebDriverWait(browser,180).until(EC.element_to_be_clickable((By.ID,"j_idt62"))).click()
 
         time.sleep(1)
-        first_dep_name = browser.find_element_by_id("dependentFirstName")
+        first_dep_name = browser.find_element(by=By.ID,value="dependentFirstName")
         first_dep_name.send_keys(list_dependents[j][0].strip())
 
         if list_dependents[j][1] is not None:
-            middle_dep_name = browser.find_element_by_id("middleInitial")
+            middle_dep_name = browser.find_element(by=By.ID,value="middleInitial")
             middle_dep_name.send_keys(list_dependents[j][1].strip())
 
-        last_dep_name = browser.find_element_by_id("dependentLastName")
+        last_dep_name = browser.find_element(by=By.ID,value="dependentLastName")
         last_dep_name.send_keys(list_dependents[j][2].strip())
         
-        DOB_dep = browser.find_element_by_id("dependentDateOfBirth")
+        DOB_dep = browser.find_element(by=By.ID,value="dependentDateOfBirth")
         DOB_dep.send_keys(list_dependents[j][3].strip())
         
         if list_dependents[j][4].lower()=='spouse':
-            relationship_dep = browser.find_element_by_id("relationship:0").click()
+            relationship_dep = browser.find_element(by=By.ID,value="relationship:0").click()
         elif list_dependents[j][4].lower()=='child':
-            relationship_dep = browser.find_element_by_id("relationship:1").click()
+            relationship_dep = browser.find_element(by=By.ID,value="relationship:1").click()
         if list_dependents[j][5].lower()=='male':
-            gender_dep = browser.find_element_by_id("gender:0").click()
+            gender_dep = browser.find_element(by=By.ID,value="gender:0").click()
         elif list_dependents[j][5].lower()=='female':
-            gender_dep = browser.find_element_by_id("gender:1").click()
+            gender_dep = browser.find_element(by=By.ID,value="gender:1").click()
         
-        save = browser.find_element_by_id("j_idt174").click()
+        save = browser.find_element(by=By.ID,value="j_idt174").click()
     
     continue_dep = WebDriverWait(browser,180).until(EC.element_to_be_clickable((By.ID,"j_idt64"))).click()
     
@@ -160,25 +160,25 @@ def Payment(list_payment,list_data):
     
     #time.sleep(7)
     temp = WebDriverWait(browser,180).until(EC.element_to_be_clickable((By.ID,"cardNumber"))) #just to load the page 
-    card_type = Select(browser.find_element_by_id("cardType"))
+    card_type = Select(browser.find_element(by=By.ID,value="cardType"))
     #card_type = Select(WebDriverWait(browser,60).until(EC.presence_of_element_located((By.ID,"cardType")))
     card_type.select_by_visible_text(list_payment[1].strip())
     
-    card_number = browser.find_element_by_id("cardNumber")
+    card_number = browser.find_element(by=By.ID,value="cardNumber")
     card_number.send_keys(list_payment[2])
     
     list_to_store_expiry_date = list_payment[3].strip().split('/')
     
-    expiry_month = Select(browser.find_element_by_id("ExpiryMonth"))
+    expiry_month = Select(browser.find_element(by=By.ID,value="ExpiryMonth"))
     expiry_month.select_by_visible_text(list_to_store_expiry_date[0])
     
-    expiry_year = Select(browser.find_element_by_id("ExpiryYear"))
+    expiry_year = Select(browser.find_element(by=By.ID,value="ExpiryYear"))
     expiry_year.select_by_visible_text(list_to_store_expiry_date[1])
     
-    cvv = browser.find_element_by_id("cvvNumber")
+    cvv = browser.find_element(by=By.ID,value="cvvNumber")
     cvv.send_keys(list_payment[4])
     
-    confirm_mail = browser.find_element_by_id("confirmEmailAddress")
+    confirm_mail = browser.find_element(by=By.ID,value="confirmEmailAddress")
     confirm_mail.send_keys(list_data[10].strip())
     
     continue_payment = WebDriverWait(browser,180).until(EC.element_to_be_clickable((By.ID,"j_idt109"))).click()
@@ -187,7 +187,7 @@ def Purchase():
     #time.sleep(5)
     if str(sheet.cell(row=r,column=32).value).lower()=='yes':
         agree = WebDriverWait(browser,180).until(EC.element_to_be_clickable((By.ID,"agree"))).click()
-        purchase = browser.find_element_by_id("pay")
+        purchase = browser.find_element(by=By.ID,value="pay")
         purchase.click()
         
 def Download():
@@ -195,7 +195,7 @@ def Download():
     try:
         recipt = WebDriverWait(browser,60).until(EC.element_to_be_clickable((By.ID,"ReceiptPDF")))
         recipt.click()
-        COI = browser.find_element_by_id("COIPDF")
+        COI = browser.find_element(by=By.ID,value="COIPDF")
         COI.click() 
     except:
         return
@@ -207,7 +207,7 @@ col = sheet.max_column
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option('prefs', {
-"download.default_directory": "/home/beleswar/Downloads/Output", #Change default directory for downloads
+"download.default_directory": "/home/beleswar/Downloads/Output_selenium", #Change default directory for downloads
 "download.prompt_for_download": False, #To auto download the file
 "download.directory_upgrade": True,
 "plugins.always_open_pdf_externally": True #It will not show PDF directly in chrome
@@ -219,7 +219,7 @@ while r<=row:
     browser.get("https://enrollmentdemo.solartis.net/Quote.xhtml")
     browser.maximize_window()
     time.sleep(2)
-    continue_button = browser.find_element_by_xpath("/html/body/div/div[2]/form/div/table/tbody/tr/td/table/tbody/tr/td/a/img")
+    continue_button = browser.find_element(by=By.XPATH,value="/html/body/div/div[2]/form/div/table/tbody/tr/td/table/tbody/tr/td/a/img")
     continue_button.click()
     
     #Enrollee data
